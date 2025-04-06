c33d904faa670cffd4641a0092b0d4f2b140dab6
kaleb746 <iankaleb746@gmail.com>
2025-04-06 04:40:24 -0600

    Added SuplierOrder service

---
+++ Model/DAO/SupplierDAO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Model.DAO
+{
+    public class SupplierDAO
+    {
+        public List<Supplier> GetByCategoryName(string categoryName)
+        {
+            using (var context = new italiapizzaEntities())
+            {
+                return (from s in context.Suppliers
+                        join c in context.SupplyCategories on s.CategorySupply equals c.SupplyCategoryID
+                        where c.CategoryName == categoryName
+                        select s).ToList();
+            }
+        }
+
+    }
+}
---
+++ Services/Dtos/SupplyCategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Services.Dtos
+{
+    [DataContract]
+    public class SupplyCategoryDTO
+    {
+        [DataMember]
+        public int SupplyCategoryID { get; set; }
+
+        [DataMember]
+        public string CategoryName { get; set; }
+    }
+}
