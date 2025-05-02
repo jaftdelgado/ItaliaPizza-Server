b10c937d33652f2c77c03e8229ecc719b0b4385e
basgomcesar <cesarbasiliogomez@gmail.com>
2025-05-01 23:08:34 -0600

    add login

---
+++ Services/SesionServices/ISesionManager.cs
@@ -0,0 +1,19 @@
+﻿using Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    [ServiceContract]
+    public interface ISesionManager
+    {
+        //Login service
+        [OperationContract]
+        PersonalDTO Login(string username, string password);
+
+    }
+}
