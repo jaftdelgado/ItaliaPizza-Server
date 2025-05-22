5a5115eb2ccae263a4e751604b5bddc833d84d7c
x3776x <abrahamvvquinto@gmail.com>
2025-05-22 14:17:12 -0600

    add Waste services

---
+++ Services/Dtos/WasteSupplyDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Services.Dtos
+{
+    [DataContract]
+    public class WasteSupplyDTO
+    {
+        [DataMember]
+        public int WasteID { get; set; }
+        [DataMember]
+        public decimal Quantity { get; set; }
+        [DataMember]
+        public int SupplyID { get; set; }
+    }
+}
